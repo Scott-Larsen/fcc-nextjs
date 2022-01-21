8f320dfda87da47ee64a657d2ced13de28f4a31d
Scott-Larsen <scott@scottlarsen.com>
2022-01-21 11:59:35 -0800

    Commit when things first started going wrong and before I tried to make changes

---
+++ pages/recipes/[slug].js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import {
+  sanityClient,
+  urlFor,
+  usePreviewSubscription,
+  PortableText,
+} from "../../lib/sanity";
+
+const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
+    _id,
+    name,
+    slug,
+    mainImage
+    ingredient[]{
+        unit,
+        wholeNumber,
+        fraction,
+        ingredient->{
+            name
+        }
+    },
+    instructions
+}`;
+
+export default function OneRecipe({ data }) {
+  const { recipe } = data;
+  return (
+    <article>
+      <h1>{recipe.name}</h1>
+      <main>
+        <img />
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+          <h3>Instructions here</h3>
+        </div>
+      </main>
+    </article>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await sanityClient.fetch(
+    `*[_type == "recipe" && defined(slug.current)]{
+            "params": {
+                "slug": slug.current
+            }
+        }`
+  );
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const recipe = await sanityClient.fetch(recipeQuery, { slug });
+  return { props: { data: { recipe } } };
+}
